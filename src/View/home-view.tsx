4bf4b374af6a2f1732755290fe9e30b23f941073
Pablo Bernardo <pablobeernardo@gmail.com>
2023-08-28 14:49:30 -0300

    projeto studioBeatriz

---
+++ src/View/home-view.tsx
@@ -0,0 +1,197 @@
+import React, { useEffect } from 'react';
+import {
+  Container,
+  Header,
+  Title,
+  Subtitle,
+  ServicesSection,
+  Service,
+  ContactSection,
+  ContactButton,
+  Footer,
+  InstagramSection,
+  InstagramLink,
+  AboutMeSection,
+  DescriptionMe,
+  TitleMe,
+  NavItem,
+  NavLink,
+  NavList,
+  NavbarContainer,
+  CourseDescription,
+  CourseItem,
+  CourseList,
+  CourseTitle,
+} from "./style/style";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import logomarca from '../assets/loo2.jpg';
+import unha2 from '../assets/unha2.jpg';
+import unha3 from '../assets/unha3.jpg';
+import unha4 from '../assets/unha4.jpg';
+import unha5 from '../assets/unha5.jpg';
+import unha7 from '../assets/unha7.jpg';
+import unha8 from '../assets/unha8.jpg';
+import curso1 from '../assets/curso1.jpg';
+import curso2 from '../assets/curso2.jpg';
+
+
+
+export default function HomePage() {
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const handleScroll = () => {
+      sections.forEach((section) => {
+        const link = document.querySelector(`.nav-link[href="#${section.id}"]`);
+        const rect = section.getBoundingClientRect();
+
+        if (link) {
+          if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+            link.classList.add("active");
+          } else {
+            link.classList.remove("active");
+          }
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
+  const handleOpenWhatsapp = () => {
+    const phoneNumber = '+5524992737045';
+    window.open(`https://wa.me/${phoneNumber}`);
+  };
+
+  return (
+    <Container>
+      <NavbarContainer>
+        <NavList>
+          <NavItem>
+            <NavLink href="#Sobre" className="nav-link">
+              Quem Sou Eu
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#Servicos" className="nav-link">
+              Nossos Serviços
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#Cursos" className="nav-link">
+              Cursos
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#Contato" className="nav-link">
+              Entre em Contato
+            </NavLink>
+          </NavItem>
+        </NavList>
+      </NavbarContainer>
+      <Header>
+        <img src={logomarca} alt='logomarca' />
+        <Title>Beatriz Studio</Title>
+        <Subtitle>Nail Designer</Subtitle>
+      </Header>
+      <AboutMeSection id='Sobre'>
+        <TitleMe>Quem Sou Eu</TitleMe>
+        <DescriptionMe>
+          Olá! Sou Beatriz, uma entusiasta apaixonada pelo mundo do design de unhas. Com anos de experiência como Nail Designer, mergulhei de cabeça na arte de transformar unhas em telas elegantes e expressivas. Minha jornada começou com uma paixão por cores, texturas e formas, e desde então venho refinando minhas técnicas para oferecer a você um serviço de manicure único.
+          <br />
+          <br />
+          Meu objetivo é criar um espaço onde cada cliente possa se sentir verdadeiramente especial. Não se trata apenas de unhas, mas de proporcionar uma experiência que eleve sua autoestima e estilo. Minhas criações vão além do esmalte e se tornam verdadeiras declarações artísticas. Trabalho com dedicação para transformar sua visita em momentos de puro encantamento.
+          <br />
+          <br />
+          A cada pincelada, busco capturar a essência e personalidade de cada cliente. Cada detalhe é uma oportunidade de expressão criativa, e eu adoro explorar novas tendências e técnicas para garantir que você saia com unhas que são uma extensão autêntica de quem você é.
+          <br />
+          <br />
+          Convido você a agendar um horário e mergulhar no mundo do design de unhas comigo. Vamos juntas transformar suas unhas em uma tela em branco para a criatividade e a beleza!
+        </DescriptionMe>
+      </AboutMeSection>
+
+      <ServicesSection id='Servicos'>
+        <TitleMe>Nossos Serviços</TitleMe>
+        <Carousel
+          showArrows={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+          autoPlay={true}
+          interval={5000}
+        >
+          <Service>
+            <img src={unha2} alt="Manicure" />
+            <img src={unha3} alt="Manicure" />
+            <img src={unha4} alt="Manicure" />
+            <TitleMe>Manicure e Pedicure</TitleMe>
+            <p>Tratamento completo para suas unhas e mãos.</p>
+          </Service>
+          <Service>
+            <img src={unha5} alt="Manicure" />
+            <img src={unha7} alt="Manicure" />
+            <img src={unha8} alt="Manicure" />
+            <TitleMe>Manicure e Pedicure</TitleMe>
+            <p>Tratamento completo para suas unhas e mãos.</p>
+          </Service>
+        </Carousel>
+      </ServicesSection>
+
+      <ContactButton style={{ marginTop: '23px' }} onClick={handleOpenWhatsapp}>Agendar horário</ContactButton>
+
+      <ServicesSection id='Cursos'>
+        <TitleMe>Nossos Cursos</TitleMe>
+        <CourseList>
+          <CourseItem>
+            <img src={curso2} alt="Manicure" />
+            <CourseTitle>Curso de Nail Designer Iniciante</CourseTitle>
+            <CourseDescription>
+              Aprenda desde as técnicas básicas até as mais avançadas de nail art. Este curso é perfeito para quem está começando e deseja se tornar um profissional qualificado.
+            </CourseDescription>
+          </CourseItem>
+          <CourseItem>
+            <img src={curso1} alt="Manicure" />
+            <CourseTitle>Curso Avançado de Nail Art</CourseTitle>
+            <CourseDescription>
+              Este curso é projetado para aprimorar suas habilidades de design e estimular sua criatividade. Você aprenderá a criar nail arts únicas e personalizadas para suas clientes.
+            </CourseDescription>
+          </CourseItem>
+        </CourseList>
+      </ServicesSection>
+
+      <ContactSection id='Contato'>
+        <TitleMe>Entre em Contato</TitleMe>
+        <p>Rua Presidente Vargas, 269 - Centro - Três Rios - RJ</p>
+        <a
+          href="https://goo.gl/maps/NHAb6i5cTyYKZzMM6"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{textDecoration: 'none'}}
+        >
+          Ver no Google Maps
+        </a>
+        <p>Agende um horário e realce a sua beleza.</p>
+        <ContactButton onClick={handleOpenWhatsapp}>Agendar Agora</ContactButton>
+      </ContactSection>
+
+      <InstagramSection>
+        <InstagramLink href="https://www.instagram.com/beatrizpereira.studio/?igshid=MzRlODBiNWFlZA%3D%3D&fbclid=IwAR2tOoGQD9JPflrO32gCq2tUZNyYw2pGTbmZRKYHCUqSdKJ5iG9Bv_Z3shY">
+          <FontAwesomeIcon icon={faInstagram} style={{ marginRight: '8px' }} />
+          Siga-nos no Instagram
+        </InstagramLink>
+      </InstagramSection>
+
+      <Footer>
+        <p>&copy; 2023 Studio de Nail Designer. Todos os direitos reservados.</p>
+      </Footer>
+    </Container>
+  );
+}
---
+++ src/View/style/style.tsx
@@ -0,0 +1,359 @@
+import styled, { keyframes } from 'styled-components';
+import Slider from 'react-slick';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+
+export const Container = styled.div`
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 20px;
+
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
+`;
+
+
+export const Header = styled.header`
+  background-color: rgb(43,41,42);
+  padding: 20px;
+
+  img {
+    max-width: 100%;
+    width: 400px;
+    height: 400px;
+    
+  }
+
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
+`;
+
+export const Title = styled.h1`
+  color: rgb(207,175,132);
+  font-family: "Montserrat";
+  margin: 0;
+`;
+
+export const Subtitle = styled.p`
+  color: rgb(207,175,132);
+  font-family: "Montserrat";
+  margin-bottom: 1;
+`;
+
+export const AboutMeSection = styled.div`
+  margin-top: 40px;
+  text-align: center;
+`;
+
+export const TitleMe = styled.h2`
+  font-size: 28px;
+  color: rgb(207,175,132);
+  margin-bottom: 20px;
+  font-family: "Montserrat";
+  animation: ${fadeIn} 1s ease; 
+
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const DescriptionMe = styled.p`
+  font-size: 16px;
+  line-height: 1.6;
+  color: #555;
+  animation: ${fadeIn} 1s ease; 
+`;
+
+
+export const ServicesSection = styled.section`
+  margin-top: 40px;
+
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+export const Service = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 40px;
+  animation: ${slideIn} 1s ease; 
+
+
+  h3 {
+    margin-top: 0;
+  }
+
+  p {
+    color: #777;
+  }
+
+  img {
+    max-width: 25%;
+    border-radius: 8px;
+    margin-right: 8px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
+`;
+
+export const Carousel = styled(Slider)`
+  .slick-prev,
+  .slick-next {
+    background-color: rgba(0, 0, 0, 0.3);
+    color: #fff;
+    border: none;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    transition: background-color 0.3s ease;
+    cursor: pointer;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+  }
+
+  .slick-prev {
+    left: -20px;
+  }
+  
+  .slick-next {
+    right: -20px;
+  }
+
+  .slick-dots {
+    bottom: 10px;
+
+    li {
+      button {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.3);
+        transition: background-color 0.3s ease;
+
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.5);
+        }
+      }
+    }
+  }
+`;
+
+export const InstagramSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const InstagramLink = styled.a`
+  background-color: rgb(207,175,132);
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 20px;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #e62068;
+  }
+`;
+
+
+
+export const ContactSection = styled.section`
+  margin-top: 40px;
+  background-color: #f5f5f5;
+  padding: 20px;
+
+  @media (max-width: 768px) {
+    margin-top: 20px;
+    padding: 10px;
+  }
+`;
+
+export const ContactButton = styled.button`
+  width: 150px;
+  height:40px;
+  background-color: rgb(207,175,132);
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease; 
+  font-size: 14px;
+
+  &:hover {
+    background-color: #ff5588; 
+  }
+
+  @media (max-width: 768px) {
+    padding: 8px 16px;
+  }
+`;
+
+
+export const Footer = styled.footer`
+  margin-top: 40px;
+  color: #888;
+
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+export const NavbarContainer = styled.nav`
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  z-index: 100;
+
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
+  `;
+
+export const NavList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  padding: 0;
+  list-style: none;
+`;
+
+export const NavItem = styled.li`
+  margin: 0 20px;
+  @media (max-width: 768px) {
+    margin: 0 10px;
+  }
+`;
+
+export const NavLink = styled.a`
+position: relative;
+text-decoration: none;
+color: #555;
+transition: color 0.3s ease-in-out;
+
+&.active {
+  color: rgb(207, 175, 132);
+}
+
+&::after {
+  content: "";
+  position: absolute;
+  bottom: -3px;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: rgb(207, 175, 132);
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 0.3s ease-in-out;
+}
+`;
+
+export const CoursesSection = styled.section`
+  background-color: #f9f9f9;
+  padding: 30px 0;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    padding: 60px 0;
+  }
+`;
+
+
+export const CourseList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  img {
+    max-width: 100%;
+    width: 450px;
+    border-radius: 8px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
+
+`;
+
+export const CourseItem = styled.li`
+  width: 100%;
+  max-width: 400px;
+  margin: 0 20px 30px;
+  background-color: #fff;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+    margin: 0 10px 20px;
+  }
+`;
+
+export const CourseTitle = styled.h3`
+  font-size: 20px;
+  color: #555;
+  margin-bottom: 10px;
+  font-family: "Montserrat";
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+export const CourseDescription = styled.p`
+  font-size: 16px;
+  color: #777;
+  line-height: 1.6;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
